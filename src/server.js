a5c3fae0ea9dbf3b7fb38bb64898d8997234641e
ALUNO I22 <ALUNOI22@unifor.local>
2025-05-19 22:13:46 -0300

    chore: adicionando primeiros arquivos e configurando vercel

---
+++ src/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + 'public/index.html');0
+});
+
+app.listen(port, () => {
+    console.log('App executado na porta ' + port);
+});
